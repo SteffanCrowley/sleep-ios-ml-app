e4fbd4831edbe176c71e6065dcf11d9fa793858c
Steffan Crowley <steffancrow@gmail.com>
2022-08-21 10:54:51 -0700

    add in ML and connect

--- Sleep-IOS-APP/ContentView.swift
+++ Sleep-IOS-APP/ContentView.swift
@@ -4,13 +4,17 @@
 //
 //  Created by steffan crowley on 8/21/22.
 //
-
+import CoreML
 import SwiftUI
 
 struct ContentView: View {
     @State private var wakeUp = Date.now
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
+    
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
     
     
     var body: some View {
@@ -31,8 +35,37 @@ struct ContentView: View {
                 Stepper(coffeeAmount == 1 ? "1 cup": "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
                 
             }
+            .navigationTitle("BetterSleep")
+            .toolbar {
+                Button("Calculate", action: calculateBedTime)
+            }
+            .alert(alertTitle, isPresented: $showingAlert) {
+                Button("OK") {}
+            } message: {
+                Text(alertMessage)
+            }
         }
-    
+        
+    }
+    func calculateBedTime() {
+        do {
+            let config = MLModelConfiguration()
+            let model = try Sleep_IOS_1(configuration: config)
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            
+            let predicition = try model.prediction(wake: Double( hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            let sleepTime = wakeUp - predicition.actualSleep
+            
+            alertTitle = "Your ideal bedtime is..."
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+            
+        } catch {
+            alertTitle = "Error"
+            alertMessage = "Sorry, there was a problem calculating your bedtime."
+        }
+        showingAlert = true
     }
 }
 
