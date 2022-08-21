9583cb4d86a47fc047a2d6d815c001564380d65a
Steffan Crowley <steffancrow@gmail.com>
2022-08-21 08:55:48 -0700

    Initial Commit

---
+++ Sleep-IOS-APP/Sleep_IOS_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sleep_IOS_APPApp.swift
+//  Sleep-IOS-APP
+//
+//  Created by steffan crowley on 8/21/22.
+//
+
+import SwiftUI
+
+@main
+struct Sleep_IOS_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
